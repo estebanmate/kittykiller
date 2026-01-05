5b5a54c028a40709229ea310f63cb89169cb36d5
Esteban Martín-Tembleque Poves <estebanmate@gmail.com>
2026-01-05 13:09:29 +0100

    Añadido Firebase

--- app/src/main/java/com/emtp/kittykiller/MotorIA.kt
+++ app/src/main/java/com/emtp/kittykiller/MotorIA.kt
@@ -31,8 +31,8 @@ object MotorIA {
             if (llmInference != null) return@withContext true
             try {
                 // Buscamos el modelo en el almacenamiento interno (descargado previamente)
-                File(context.filesDir, "gemma-2b-it-cpu-int4.bin")
-                val modelFile = File(context.filesDir, "gemma-2-2b-it-cpu-int8.task")
+                val modelFile = File(context.filesDir, "gemma-2b-it-cpu-int4.bin")
+//                val modelFile = File(context.filesDir, "gemma-2-2b-it-cpu-int8.task")
                 //if (!modelFile.exists()) return@withContext false
 
                 val options = LlmInference.LlmInferenceOptions.builder()
